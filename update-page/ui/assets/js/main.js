91c3ce34615d88f93820316c042b5bd497c2f5d9
Andrej Pedchenko <andrej.cdo@gmail.com>
2025-02-13 20:41:47 +0200

    feature/auto-update-content
    Checkpoint

--- update-page/ui/assets/js/main.js
+++ update-page/ui/assets/js/main.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', function () {
     console.log("Page is fully loaded.");
 
+    loadMatches(`/api/matches/live`);
+
     // set footer text
     const dateFooter = document.querySelector('footer .js-date-container');
     const dateOptions = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
